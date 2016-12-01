f027692ad31003953083adef046aa47f678234ea
emeric254 <emeric254@live.fr>
2016-12-01 23:13:34 +0100

    first draft

--- Exemples/angular material/RSC/JS/myapp.js
+++ Exemples/angular material/RSC/JS/myapp.js
@@ -1,7 +1,7 @@
 //
 var myapp = angular.module('Uploader', ['ngMaterial', 'ngAnimate']);
 //
-myapp.controller('mainControl', function($scope, $http, $mdDialog, $location) {
+myapp.controller('mainControl', function($scope, $http, $mdDialog, $mdSidenav, $location) {
     // static vars :
     $scope.app = {};
     $scope.app.name = "Pas de toit pas de chocolat";
@@ -9,8 +9,17 @@ myapp.controller('mainControl', function($scope, $http, $mdDialog, $location) {
     $scope.app.description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamc";
     $scope.app.url = $location.absUrl();
     $scope.app.project_source = "https://github.com/Nuit-De-L-Info-2016-STRI-DL";
+
     // other vars :
     $scope.loading = 0;  // loading bar state (0: disabled, !=0 : there is a request pending)
+
+    // menu
+    $scope.toggleLeft = buildToggler('left');
+    function buildToggler(componentId) {
+      return function() {
+        $mdSidenav(componentId).toggle();
+      }
+    };
 });
 
 // IE support
