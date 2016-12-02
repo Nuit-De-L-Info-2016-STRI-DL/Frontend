97250470c91dd348a36b7290c3f9dbfed4b57b69
Camy <miss.Caamy@gmail.com>
2016-12-02 02:28:54 +0100

    SuperMapDeOuf : Elle détecte la localisation de la personne connectée à l'appli

--- static/RSC/JS/mapIntegration.js
+++ static/RSC/JS/mapIntegration.js
@@ -3,13 +3,17 @@
  */
 
 // TODO Fonction qui récupère Lat et Long pour l'API googlE.
-
-function initMap() {
-    // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 43.600000, lng: 1.433333},
-        scrollwheel: false,
-        zoom: 8
-    });
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            // Create a map object and specify the DOM element for display.
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: position.coords.latitude, lng: position.coords.longitude},
+                scrollwheel: true,
+                zoom: 15
+            });
+        });
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
 }
-
